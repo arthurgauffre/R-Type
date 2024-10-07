efe72b13e0304850210f50183816458d3935f397
arthurgauffre <arthurgauffre@users.noreply.github.com>
2024-10-07 16:30:17 +0000

    Automated Change

--- app/ecs/src/components/hitbox/HitBoxComponent.cpp
+++ app/ecs/src/components/hitbox/HitBoxComponent.cpp
@@ -7,25 +7,14 @@
 
 #include <components/HitBoxComponent.hpp>
 
-component::HitBoxComponent::HitBoxComponent(uint32_t entityID, float width, float height)
-    : AComponent(entityID), _width(width), _height(height)
-{
-}
+component::HitBoxComponent::HitBoxComponent(uint32_t entityID, float width,
+                                            float height)
+    : AComponent(entityID), _width(width), _height(height) {}
 
-component::HitBoxComponent::~HitBoxComponent()
-{
-}
+component::HitBoxComponent::~HitBoxComponent() {}
 
-float component::HitBoxComponent::getWidth() const
-{
-    return _width;
-}
+float component::HitBoxComponent::getWidth() const { return _width; }
 
-float component::HitBoxComponent::getHeight() const
-{
-    return _height;
-}
+float component::HitBoxComponent::getHeight() const { return _height; }
 
-void component::HitBoxComponent::update(float deltaTime)
-{
-}
+void component::HitBoxComponent::update(float deltaTime) {}
