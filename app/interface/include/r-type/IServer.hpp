870c140f0b1c059e9948e60ce08af03949ab0ff4
JsuisSayker <JsuisSayker@users.noreply.github.com>
2024-10-13 21:46:16 +0000

    Automated Change

--- app/interface/include/r-type/IServer.hpp
+++ app/interface/include/r-type/IServer.hpp
@@ -15,29 +15,37 @@
 #include <NetworkingCommon.hpp>
 
 namespace rtype {
-    namespace network {
-    template <typename T>
-    class IServer {
-        public:
-            IServer(){};
-            virtual ~IServer(){};
+namespace network {
+template <typename T> class IServer {
+public:
+  IServer(){};
+  virtual ~IServer(){};
 
-            virtual bool Start() = 0;
-            virtual void Stop() = 0;
+  virtual bool Start() = 0;
+  virtual void Stop() = 0;
 
-            virtual void WaitForMessage() = 0;
+  virtual void WaitForMessage() = 0;
 
-            virtual void SendMessageToClient(const rtype::network::Message<T> &message, std::shared_ptr<rtype::network::NetworkConnection<T>> client) = 0;
-            virtual void SendMessageToAllClients(const rtype::network::Message<T> &message, std::shared_ptr<rtype::network::NetworkConnection<T>> clientToIgnore = nullptr) = 0;
-            virtual void Update(size_t maxMessages = -1, bool needToWait = false) = 0;
-            virtual void OnClientValidated(std::shared_ptr<rtype::network::NetworkConnection<T>> client) = 0;
+  virtual void SendMessageToClient(
+      const rtype::network::Message<T> &message,
+      std::shared_ptr<rtype::network::NetworkConnection<T>> client) = 0;
+  virtual void SendMessageToAllClients(
+      const rtype::network::Message<T> &message,
+      std::shared_ptr<rtype::network::NetworkConnection<T>> clientToIgnore =
+          nullptr) = 0;
+  virtual void Update(size_t maxMessages = -1, bool needToWait = false) = 0;
+  virtual void OnClientValidated(
+      std::shared_ptr<rtype::network::NetworkConnection<T>> client) = 0;
 
-            protected:
-            virtual void OnMessageReceived(std::shared_ptr<NetworkConnection<T>> client, rtype::network::Message<T> &message) = 0;
-            virtual bool OnClientConnection(std::shared_ptr<rtype::network::NetworkConnection<T>> client) = 0;
-            virtual void OnClientDisconnection(std::shared_ptr<rtype::network::NetworkConnection<T>> client) = 0;
-        };
-    }
-}
+protected:
+  virtual void OnMessageReceived(std::shared_ptr<NetworkConnection<T>> client,
+                                 rtype::network::Message<T> &message) = 0;
+  virtual bool OnClientConnection(
+      std::shared_ptr<rtype::network::NetworkConnection<T>> client) = 0;
+  virtual void OnClientDisconnection(
+      std::shared_ptr<rtype::network::NetworkConnection<T>> client) = 0;
+};
+} // namespace network
+} // namespace rtype
 
 #endif /* !ISERVER_HPP_ */
