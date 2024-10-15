981243291feca5e38dfa6e52451359d4abfb6ab1
OxiiLB <OxiiLB@users.noreply.github.com>
2024-10-15 23:39:50 +0000

    Automated Change

--- interface/include/systems/GameSystem.hpp
+++ interface/include/systems/GameSystem.hpp
@@ -7,27 +7,25 @@
 
 #pragma once
 
-#include <r-type/ASystem.hpp>
 #include <Error.hpp>
+#include <r-type/ASystem.hpp>
 
 #include <components/TypeComponent.hpp>
 
-namespace ECS_system
-{
-    class GameSystem : virtual public ASystem
-    {
-    public:
-        GameSystem(component::ComponentManager &componentManager,
-                   entity::EntityManager &entityManager);
-        ~GameSystem();
+namespace ECS_system {
+class GameSystem : virtual public ASystem {
+public:
+  GameSystem(component::ComponentManager &componentManager,
+             entity::EntityManager &entityManager);
+  ~GameSystem();
 
-        void update(float deltaTime,
-                    std::vector<std::shared_ptr<entity::IEntity>> entities) override;
+  void update(float deltaTime,
+              std::vector<std::shared_ptr<entity::IEntity>> entities) override;
 
-        void initialize() override {}
-        void handleComponents() override {}
+  void initialize() override {}
+  void handleComponents() override {}
 
-    protected:
-    private:
-    };
-}
+protected:
+private:
+};
+} // namespace ECS_system
