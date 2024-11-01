374b7a825d62e51db1c29e5053e27b0110f36249
JsuisSayker <killian.trouve@epitech.eu>
2024-11-01 23:48:33 +0100

    [UPD] TextComponent.cpp
    Remove a useless debug comment and add the comment of the namespace

--- app/ecs/src/components/text/TextComponent.cpp
+++ app/ecs/src/components/text/TextComponent.cpp
@@ -10,7 +10,6 @@
 namespace component {
 TextComponent::TextComponent(uint32_t entityID, std::pair<float, float> position, std::string text, int size, RColor color, std::string fontPath, std::shared_ptr<IGraphic> graphic)
     : AComponent(entityID), _x(position.first), _y(position.second), _text(text), _size(size), _color(color), _fontPath(fontPath){
-        // std::cout << "x: " << position.first << " y: " << position.second << " text: " << text << " size: " << size << " color: " << color.r << " " << color.g << " " << color.b << " " << color.a << " fontPath: " << fontPath << std::endl;
         _textId = graphic->createText(text, fontPath, size, color);
         graphic->setTextPosition(position.first, position.second, _textId);
     };
@@ -25,4 +24,4 @@ TextComponent::TextComponent(uint32_t entityID, std::pair<float, float> position
         graphic->updateText(_textId, text, size, color);
         graphic->setTextPosition(_textId, position.first, position.second);
     }
-}
+} // namespace component
