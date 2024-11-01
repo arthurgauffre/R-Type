fa09207757560a765f81376f0811ea01bf9393cf
JsuisSayker <killian.trouve@epitech.eu>
2024-11-01 23:43:00 +0100

    [UPD] ComponentManager.cpp
    Remove a useless method and the doc attached to it, because the method was unexistant in the hpp file

--- app/ecs/src/managers/component/ComponentManager.cpp
+++ app/ecs/src/managers/component/ComponentManager.cpp
@@ -8,22 +8,3 @@
 #include <managers/ComponentManager.hpp>
 
 component::ComponentManager::ComponentManager() {}
-
-/**
- * @brief Updates all active components for each entity.
- *
- * This function iterates through all the components of each entity and calls
- * their update method if they are active. The update method is called with
- * the given delta time.
- *
- * @param deltaTime The time elapsed since the last update, used to update the
- * components.
- */
-// void component::ComponentManager::update(float deltaTime) {
-//   for (auto &entityComponents : _components) {
-//     for (auto &component : entityComponents.second) {
-//       if (component->getActive())
-//         component->update(deltaTime);
-//     }
-//   }
-// }
